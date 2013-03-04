93a6a7c57ad06038e609b8d1572943ffa606b201
arisfatahillah <arisfatahillah@gmail.com>
2013-03-05 06:09:15 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 tes coba wkwkwkwkW ini adalah halaman windows
+ini yg berhasil 
 http://blog.novalagung.com/2012/08/git-vcs-dengan-github-repository.html
+=====
+http://annisapuspakirana.staff.ub.ac.id/tutorial-instalasi-git/
