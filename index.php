122dd25c3eac9df33684a980d72d7a024f989ff3
arisfatahillah <arisfatahillah@gmail.com>
2013-03-06 06:07:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,7 @@ ini yg berhasil
 http://blog.novalagung.com/2012/08/git-vcs-dengan-github-repository.html
 =====
 http://annisapuspakirana.staff.ub.ac.id/tutorial-instalasi-git/
+
+https://github.com/sferik/rails_admin
+http://www.webappers.com/2011/06/08/admin-style-interfaces-for-ruby-on-rails/
+http://ruby-on-rails.wonderhowto.com/how-to/add-basic-crud-ruby-rails-3-application-405786/
