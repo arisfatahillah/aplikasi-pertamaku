f70754ba719891815ea8c2be0b191c27af93e6c6
arisfatahillah <arisfatahillah@gmail.com>
2013-03-06 06:25:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,3 +8,4 @@ https://github.com/sferik/rails_admin
 http://www.webappers.com/2011/06/08/admin-style-interfaces-for-ruby-on-rails/
 http://ruby-on-rails.wonderhowto.com/how-to/add-basic-crud-ruby-rails-3-application-405786/
 http://www.developer.com/db/article.php/10920_3656831_2/Skip-the-CRUD-Building-a-Simple-Database-Backed-Web-Application-Using-Ruby-on-Rails.htm
+http://activescaffold.com/
